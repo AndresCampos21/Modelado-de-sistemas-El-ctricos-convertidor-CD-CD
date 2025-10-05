3eeeb3a0c115e12824f2170089033f24e2cbc9cf
AndresCampos21 <andres.campos6485@alumnos.udg.mx>
2025-10-05 10:55:56 -0600

    Update din_cd.m

--- din_cd.m
+++ din_cd.m
@@ -3,7 +3,7 @@ function dx = din_cd(t,x)
 L = 0.002;
 R = 10;
 C = 10e-6;
-U = 32;
+U = 30.6;
 T_pwm = 1/100e3; 
 duty_cycle = 0.4;
 
@@ -15,4 +15,5 @@ d = @(t) (mod(t, T_pwm) < duty_cycle * T_pwm);
 
 dx = zeros(2,1);
 dx(1) = -(1/L)*x(2)+(U/L)*d(t);
+
 dx(2) = (1/C)*x(1)-(1/(R*C))*x(2);
